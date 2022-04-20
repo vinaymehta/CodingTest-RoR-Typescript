fed13c89de02c8b77bafdd3693202306ec420c77
vinaymehta <vinay.ymca@gmail.com>
2022-04-20 16:09:28 +0530

    the response of the api is used as state params

--- app/javascript/components/TodoList/index.tsx
+++ app/javascript/components/TodoList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, ListGroup, Form } from "react-bootstrap";
 import { ResetButton } from "./uiComponent";
 import axios from "axios";
@@ -14,12 +14,19 @@ type Props = {
 };
 
 const TodoList: React.FC<Props> = ({ todoItems }) => {
+
+  // set the items in a state variable to be updated after check/uncheck
+
+  const [items, setItems] = useState(todoItems)
+
   useEffect(() => {
     const token = document.querySelector(
       "[name=csrf-token]"
     ) as HTMLMetaElement;
     axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
   }, []);
+
+  // On checkbox toggle, stop the reload and set the state variable
 
   const checkBoxOnCheck = (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -28,18 +35,19 @@ const TodoList: React.FC<Props> = ({ todoItems }) => {
     axios.post("/todo", {
       id: todoItemId,
       checked: e.target.checked,
-    });
+    }).then((res) => setItems(res?.data?.data));
   };
 
   const resetButtonOnClick = (): void => {
     axios.post("/reset").then(() => location.reload());
   };
 
+  //  use the state variables
   return (
     <Container>
       <h3>2022 Wish List</h3>
       <ListGroup>
-        {todoItems.map((todo) => (
+        {items.map((todo) => (
           <ListGroup.Item key={todo.id}>
             <Form.Check
               type="checkbox"
